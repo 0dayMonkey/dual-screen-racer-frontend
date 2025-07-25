5901af9df35d004e9965efad25f65efafc824603
Harib Naïm <naim.harib@modulusgroup.eu>
2025-07-25 10:27:26 +0200

    update LobbyScene.js

--- js/scenes/LobbyScene.js
+++ js/scenes/LobbyScene.js
@@ -152,7 +152,7 @@ class LobbyScene extends Phaser.Scene {
         
         car.setAngle(90);
         car.x = -100;
-        nameText.x = this.scale.width + 100;
+        nameText.x = -100;
         
         this.tweens.add({ targets: [car, nameText], x: this.scale.width / 2 - 50, ease: 'Cubic.easeOut', duration: 800 });
     }
