7c5431f50592d80f454b11df3a0ac73ffa8ff283
Harib Naïm <naim.harib@modulusgroup.eu>
2025-07-25 12:55:03 +0200

    update Player.js

--- js/components/Player.js
+++ js/components/Player.js
@@ -15,9 +15,25 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         
         this.playerId = playerData.id;
         this.name = playerData.name;
-        this.targetAngle = 0; // Angle cible, contrôlé par la manette
+        this.targetAngle = 0;
         this.score = 0;
         this.offScreenSince = null;
+
+        // NOUVEAU : Création de l'émetteur de particules de fumée
+        this.smokeEmitter = scene.add.particles(0, 0, 'smoke_particle', {
+            speed: { min: 10, max: 30 },
+            angle: { min: 85, max: 95 }, // Dirigé vers l'arrière de la voiture
+            scale: { start: 1, end: 0 },
+            alpha: { start: 0.4, end: 0 },
+            lifespan: 400,
+            frequency: 80, // Émission plus ou moins continue quand activé
+            blendMode: 'NORMAL'
+        });
+
+        // Attache l'émetteur au joueur avec un décalage pour sortir de l'arrière
+        // Le décalage est appliqué par rapport au centre du sprite non-rotaté
+        this.smokeEmitter.startFollow(this, 0, this.height / 2);
+        this.smokeEmitter.emitting = false; // Commence désactivé
     }
 
     updateMovement() {
@@ -26,10 +42,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
         const roadLeftBoundary = this.scene.scale.width * 0.15;
         const roadRightBoundary = this.scene.scale.width * 0.85;
-        const outerLeftBoundary = this.scene.scale.width * 0.075;
-        const outerRightBoundary = this.scene.scale.width * 0.925;
 
-        // Logique de ralentissement
         if (this.x < roadLeftBoundary || this.x > roadRightBoundary) {
             currentSpeed = forwardSpeed * 0.6;
             this.setDrag(this.grassDrag);
@@ -38,14 +51,25 @@ class Player extends Phaser.Physics.Arcade.Sprite {
             this.setDrag(this.roadDrag);
         }
 
-        // Logique de rotation
-        // On interpole l'angle actuel vers l'angle cible pour un mouvement fluide
         this.angle = Phaser.Math.Linear(this.angle, this.targetAngle, 0.1);
-
-        // Appliquer la vitesse en fonction de l'angle
         this.scene.physics.velocityFromAngle(this.angle - 90, currentSpeed, this.body.velocity);
+        
+        // Appliquer les murs invisibles (si nécessaire, semble être géré par world bounds)
+        // this.x = Phaser.Math.Clamp(this.x, outerLeftBoundary, outerRightBoundary);
 
-        // Appliquer les murs invisibles
-        this.x = Phaser.Math.Clamp(this.x, outerLeftBoundary, outerRightBoundary);
+        // NOUVEAU : Gère l'émission de fumée en fonction de la vitesse
+        if (this.body.velocity.length() > 200) {
+            this.smokeEmitter.emitting = true;
+        } else {
+            this.smokeEmitter.emitting = false;
+        }
+    }
+
+    // MODIFIÉ : S'assurer que l'émetteur est détruit avec le joueur
+    destroy(fromScene) {
+        if (this.smokeEmitter) {
+            this.smokeEmitter.destroy();
+        }
+        super.destroy(fromScene);
     }
 }
